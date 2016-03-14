f9eb6c4d0a345b00dd59e75f5124a1bd8d3fcb6d
Momchil <gorchev@MGorchev.local>
2016-03-14 23:15:04 +0000

    Example of nested components

--- app/index.js
+++ app/index.js
@@ -5,11 +5,37 @@ var App = React.createClass({
     render: function(){
         return (
             <div className="app-body">
-                Hello
+                <Content />
             </div>
         )
     }
 });
+
+var Content = React.createClass({
+    render: function(){
+        var arr = ["some", "values", "in", "list"];
+        return (
+            <div>
+                <h3>The list</h3>
+                <List items={arr} />
+            </div>
+        )
+    }
+});
+
+var List = React.createClass({
+    render: function(){
+        var listItems = this.props.items.map(function(item){
+            return <li>{item}</li>
+        });
+
+        return (
+            <ul>
+                {listItems}
+            </ul>
+        )
+    }
+})
 
 ReactDOM.render(
     <App />,
