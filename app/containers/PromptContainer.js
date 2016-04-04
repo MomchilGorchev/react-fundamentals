e968d933fd9b0961f72365849841b77b375925db
Momchil Gorchev <momchil.gorchev@gmail.com>
2016-04-05 00:13:58 +0100

    refactoring components

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { transparentBg } from '../styles/index'
+import Prompt from '../components/Prompt'
 	
 class PromptContainer extends React.Component {
 
@@ -12,7 +12,7 @@ class PromptContainer extends React.Component {
 		};
 
 		this.handleChange = this.handleChange.bind(this);
-		this.submitHandle = this.submitHandle.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}	
 
 	handleChange(e){
@@ -23,7 +23,7 @@ class PromptContainer extends React.Component {
 
 	}
 
-	submitHandle(){
+	handleSubmit(){
 
 		let username = this.state.username;
 
@@ -48,29 +48,12 @@ class PromptContainer extends React.Component {
 
 	render(){
 		return (
-			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" styles={transparentBg}>
-		      <h1>{this.props.route.header}</h1>
-		      <div className="col-sm-12">
-		        <form onSubmit={this.submitHandle}>
-		          <div className="form-group">
-		            <input
-		              className='form-control'
-		              placeholder='Github Username'
-		              onChange={this.handleChange}
-		              value={this.state.username}
-		              type='text'
-		              />
-		          </div>
-		          <div className="form-group col-sm-4 col-sm-offset-4">
-		            <button
-		              className="btn btn-block btn-success"
-		              type="submit">
-		                Continue
-		            </button>
-		          </div>
-		        </form>
-		      </div>
-		    </div>
+			<Prompt 
+				onSubmitUser={this.handleSubmit}
+				onUpdateUser={this.handleChange}
+				header={this.props.route.header}
+				username={this.state.username}
+			/>
 		)
 	}
 }
