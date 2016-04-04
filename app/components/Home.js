1e19e476942c89802be770e749daf70a14743a8b
Momchil Gorchev <momchil.gorchev@gmail.com>
2016-04-04 11:10:43 +0100

    Experimenting with router

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import Styles from '../styles/index'
+import { Link } from 'react-router'
 
 
 class Home extends React.Component {
 
 	render() {
 		return (
-			<div>
-				Hello from Home!
-			</div>
+			<div className="jumbotron col-sm-12 text-center" style={Styles.transparentBg}>
+				<h1>Github Battle</h1>
+				<p className='lead'>What even is a jQuery?</p>
+				<Link to='/playerOne'>
+					<button type='button' className='btn btn-lg btn-success'>Get Started</button>
+				</Link>
+		    </div>
 		)
 	}
 }
